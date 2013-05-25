6381576aa20c136a7b2a1ed61ecf3a2630d567bd
michael-ns <houyuxi@hotmail.com>
2013-05-25 21:35:42 +1000

    before create signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Auto::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  resources :microposts
-
-  resources :users
+  get "users/new"
+  root  to: 'static_pages#home'
+  match '/help',  to: 'static_pages#help',  via: 'get'
+  resources :users, :microposts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
