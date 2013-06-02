c94565f61292a9d331c252b45bfe9e0f4d4c6dd5
michael-ns <houyuxi@hotmail.com>
2013-06-02 23:23:15 +1000

    add create feature function

--- app/controllers/feature_controller.rb
+++ app/controllers/feature_controller.rb
@@ -1,15 +1,19 @@
 class FeatureController < ApplicationController
-	def list
-		@features = Feature.all
-	end
-
 	def create
-		@feature = Feature.new(user_params)
+		@feature = Feature.new(feature_params)
 
 		respond_to do |format|
-			if @feature.save
-				render :partial => 'subject', :object => @subject
-			end
-		end
-	end	
+	      if @feature.save
+	        format.html { redirect_to root_path, notice: 'feature was successfully created.' }
+	        format.json { render json: @feature, status: :created, location: @feature }
+	      else
+	        format.html { render action: "new" }
+	        format.json { render json: @feature.errors, status: :unprocessable_entity }
+	      end
+	    end
+	end
+
+	def feature_params
+      params.require(:feature).permit(:name)
+    end
 end
